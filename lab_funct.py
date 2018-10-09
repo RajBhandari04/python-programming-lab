e0a101c15299e7c14be5100e430e7c960a0c2312
RajBhandari04 <43983182+RajBhandari04@users.noreply.github.com>
2018-10-09 13:52:39 +0530

    Update lab_funct.py

--- lab_funct.py
+++ lab_funct.py
@@ -1,14 +1,14 @@
-def ArmN(x):
+def ArmN(x): #function declaration
  sum=0
  t=x
- while(t>0):
-  d=t%10
-  sum+=d**3
-  t=t//10
- if sum==x:
+ while(t>0): #condition statement
+  d=t%10     #extracts last digit
+  sum+=d**3  #adds the cube
+  t=t//10    #removes remaining digits
+ if sum==x:  #checks the conditions 
   return 'Armstrong number'
  else:
   return 'not armstrong'
-x=int(input())
-print(ArmN(x))
+x=int(input()) #input statement
+print(ArmN(x)) #print statement
 
